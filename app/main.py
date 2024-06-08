00b8c24dedbf2e6c33c4822cd187d72bd210aede
goodhamgupta <shubhamg2208@live.com>
2024-06-08 16:10:47 +0800

    Add moar logs

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@ import os
 import shutil
 
 def create_tmp_dir():
+    print("Old root: ", os.getcwd())
     os.mkdir("/tmp/runner")
     shutil.copy("/usr/local/bin/docker-explorer", "/tmp/runner/docker-explorer")
     os.chroot("/tmp/runner")
+    print("New root: ", os.getcwd())
 
 
 def main():
