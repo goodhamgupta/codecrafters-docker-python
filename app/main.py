0dd9e6364ed7cacf609a84e875f6e05b6d1464ce
goodhamgupta <shubhamg2208@live.com>
2024-06-08 15:45:17 +0800

    Remove stderr logs

--- app/main.py
+++ app/main.py
@@ -6,10 +6,7 @@ def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
-    # print(completed_process.stderr.decode("utf-8"))
-
-
+    print(completed_process.stdout.decode("utf-8").strip())
 
 if __name__ == "__main__":
     main()
