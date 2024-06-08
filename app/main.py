036d8e773201f808d9857c9d6a2b99c7c1c11f2b
goodhamgupta <shubhamg2208@live.com>
2024-06-08 16:01:58 +0800

    Create tmp dir and chroot into it

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 import subprocess
 import sys
+import os
 
+def create_tmp_dir():
+    os.mkdir("/tmp/runner")
+    os.chroot("/tmp/runner")
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
+    create_tmp_dir()
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8").strip())
     sys.stderr.write(completed_process.stderr.decode("utf-8"))
