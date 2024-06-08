aa541eccabe63c7bf3983031c28e137995069225
goodhamgupta <shubhamg2208@live.com>
2024-06-08 16:03:59 +0800

    Add symlink

--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ def create_tmp_dir():
     os.mkdir("/tmp/runner")
     os.chroot("/tmp/runner")
     shutil.copy("/usr/local/bin/docker-explorer", "/tmp/runner/docker-explorer")
+    os.symlink("/usr/local/bin/docker-explorer", "/tmp/runner/docker-explorer")
+
 
 def main():
     command = sys.argv[3]
